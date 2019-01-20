71810c4925ef99fd4c867fdf9bbfd33c95e03a6f
robertkeithrippetoe <rkrippetoe@gmail.com>
2019-01-20 14:37:28 -0700

    Add input handling example

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+)
+
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("What is your name?")
+	var text string
+	fmt.Scanln(&text)
+	switch text {
+	case "Keith":
+		fmt.Println("Wow, do you listen to internet radio!?")
+	case "Mike":
+		fmt.Println("Wow, do you know about K&M!?")
+	default:
+		fmt.Println("I don't care.")
+	}
 }
