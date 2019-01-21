854c80a177864ae7e94666bdb83c2a2fa9f54ae9
robertkeithrippetoe <rkrippetoe@gmail.com>
2019-01-20 19:24:40 -0700

    Import probs

--- main.go
+++ main.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"fmt"
+	//draft "github.com/GolangBoardGame/Draft"
 )
 
 func main() {
 	fmt.Println("Now Booting Game")
-	/*fmt.Println("What is your name?")
-	var text string
-	fmt.Scanln(&text)
-	switch text {
-	case "Keith":
-		fmt.Println("Wow, do you listen to internet radio!?")
-	case "Mike":
-		fmt.Println("Wow, do you know about K&M!?")
-	default:
-		fmt.Println("I don't care.")
-	}
+	//var character1 draft.Character
+
+	//character1 = draft.GenerateCharacter
+	//fmt.Println("Say hi to " + character1.name)
+	/*
+		fmt.Println("What is your name?")
+		var text string
+		fmt.Scanln(&text)
+		switch text {
+		case "Keith":
+			fmt.Println("Wow, do you listen to internet radio!?")
+		case "Mike":
+			fmt.Println("Wow, do you know about K&M!?")
+		default:
+			fmt.Println("I don't care.")
+		}
 	*/
 }
