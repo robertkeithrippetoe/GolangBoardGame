0ad13752a94e9d26f68460e9e99dc7913f14c8a6
robertkeithrippetoe <rkrippetoe@gmail.com>
2019-01-20 18:29:35 -0700

    add character generation

--- Draft/draft.go
+++ Draft/draft.go
@@ -1,9 +1,55 @@
 package draft
 
-type character struct {
+import (
+	"math/rand"
+)
+
+type Character struct {
 	name      string
 	speed     int
 	strength  int
 	health    int
 	endurance int
+}
+
+func generateCharacter() Character {
+	var character Character
+	var firstName string
+	var lastName string
+	r := rand.Intn(10) + 1
+	character.speed = r
+	r = rand.Intn(10) + 1
+	character.strength = r
+	r = rand.Intn(10) + 1
+	character.health = r
+	r = rand.Intn(10) + 1
+	character.endurance = r
+	r = rand.Intn(5) + 1
+	switch r {
+	case 1:
+		firstName = "Holly"
+	case 2:
+		firstName = "Keith"
+	case 3:
+		firstName = "Mike"
+	case 4:
+		firstName = "Jasper"
+	case 5:
+		firstName = "Alex"
+	}
+	r = rand.Intn(5) + 1
+	switch r {
+	case 1:
+		lastName = "Christofferson"
+	case 2:
+		lastName = "Brackenwald"
+	case 3:
+		lastName = "Chimboovist"
+	case 4:
+		lastName = "Swimtoodler"
+	case 5:
+		lastName = "Jistastimbolt"
+	}
+	character.name = firstName + lastName
+	return character
 }
